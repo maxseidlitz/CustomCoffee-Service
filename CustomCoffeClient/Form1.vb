5fb7a572d3de0ea0a99eab612607e3f49168a18b
maxseidlitz <61022094+maxseidlitz@users.noreply.github.com>
2021-11-18 17:54:49 +0100

    Anpassung Test-Client: LadenFunktion realisiert // Service lässt sich komischerweise immernoch nicht einbauen

--- CustomCoffeClient/Form1.vb
+++ CustomCoffeClient/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class frmAlleKreationen
+    Private Sub dgvKreationen_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvKreationen.CellContentClick
+
+    End Sub
+
+    Private Sub btnLaden_Click(sender As Object, e As EventArgs) Handles btnLaden.Click
+
+    End Sub
+
+    Private Sub laden()
+        Dim KreationenListe As List(Of CustomCoffeeService.Kreationen)
+        Dim svc As CustomCoffeeService.ICustomCoffeeService
+
+        svc = New CustomCoffeeService.CustomCoffeeClient
+
+        KreationenListe = svc.GibMeineKreationen
+        Me.dgvKreationen.DataSource = KreationenListe
+    End Sub
 
 End Class
