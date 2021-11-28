03e2cfeae04f8ee2701d76752ed17938c9fa9eea
maxseidlitz <61022094+maxseidlitz@users.noreply.github.com>
2021-11-28 14:38:29 +0100

    SUb Laden im Client wegen Fehlern auskommentiert

--- CustomCoffeClient/Form1.vb
+++ CustomCoffeClient/Form1.vb
@@ -4,17 +4,15 @@
     End Sub
 
     Private Sub btnLaden_Click(sender As Object, e As EventArgs) Handles btnLaden.Click
-
+        'Call btnLaden()
     End Sub
 
-    Private Sub laden()
-        Dim KreationenListe As List(Of CustomCoffeeService.Kreationen)
-        Dim svc As CustomCoffeeService.ICustomCoffeeService
-
-        svc = New CustomCoffeeService.CustomCoffeeClient
-
-        KreationenListe = svc.GibMeineKreationen
-        Me.dgvKreationen.DataSource = KreationenListe
-    End Sub
+    ' Private Sub laden()
+    'Dim KreationenListe As List(Of CustomCoffeeService.Kreationen)
+    'Dim svc As CustomCoffeeService.ICustomCoffeeService
+    '        svc = New CustomCoffeeService.CustomCoffeeClient
+    '        KreationenListe = svc.GibMeineKreationen
+    'Me.dgvKreationen.DataSource = KreationenListe
+    'End Sub
 
 End Class
